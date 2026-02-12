9986cc8604b8e0a96d9540bc7b08a8ddc3fdf812
Kaushalya <jb740le@gmail.com>
2026-02-12 23:11:28 +0530

    About Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import gsap from 'gsap';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
+import About from './components/About';
 
 
 
@@ -19,7 +20,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Cocktails />
-     
+      <About />
       
     </main>
   )
