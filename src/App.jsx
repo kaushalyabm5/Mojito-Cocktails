d3c0e2dbbc2a3b93dfbabcddef2ace2896f868c5
Kaushalya <jb740le@gmail.com>
2026-02-15 07:40:53 +0530

    menu section & contact section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
 import About from './components/About';
 import Art from './components/Art';
+import Menu from './components/Menu';
+import Contact from './components/Contact';
 
 
 
@@ -23,6 +25,8 @@ const App = () => {
       <Cocktails />
       <About />
       <Art />
+      <Menu />
+      <Contact />
       
     </main>
   )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -108,6 +108,7 @@ const Hero = () => {
 
         <div className="video absolute inset-0">
             <video
+            
                 ref={videoRef} 
                 src="/videos/output.mp4"
                 muted
